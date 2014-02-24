d249cfd393b4add17ee18719277d60f880410909
Pablo Torres <tn.pablo@gmail.com>
2014-02-24 13:52:47 -0500

    Generate boolean expressions

--- src/guess/core.clj
+++ src/guess/core.clj
@@ -1,10 +1,15 @@
 (ns guess.core
-  (:require [guess.gen-arith :as arith]))
+  (:require [guess.gen-arith :as arith]
+            [guess.gen-bool :as bool]))
 
 (def arithops [+ - * /])
+(def compops [< =])
 
 (defn -main [& args]
-  (println (take 10 (arith/all :ops arithops
-                               :max-nesting 3
-                               :max-n 5
-                               :n-vars 3))))
+  (let [nesting 3]
+    (println (take 10 (bool/all :ops compops
+                                :max-nesting nesting
+                                :arith-exps (arith/all :ops arithops
+                                                       :max-nesting nesting
+                                                       :max-n 5
+                                                       :n-vars 3))))))
---
+++ src/guess/gen_bool.clj
@@ -0,0 +1,22 @@
+(ns guess.gen-bool)
+
+(defn commutative? [op]
+  (#{=} op))
+
+(defn- build [op x y]
+  (when-not (= x y)
+    [op x y]))
+
+(defn all
+  [& {:keys [ops max-nesting arith-exps]}]
+  (let [raw-exps (mapcat(fn [op]
+                          (mapcat (fn [exp1]
+                                    (map (fn [exp2]
+                                           (build op exp1 exp2))
+                                         (if (commutative? op)
+                                           (drop (+ 1 (.indexOf arith-exps exp1))
+                                                 arith-exps)
+                                           arith-exps)))
+                                  arith-exps))
+                        ops)]
+    (remove nil? (distinct raw-exps))))
