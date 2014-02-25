10c5fdd52951f5bb745acdf7b23db68f09fbec71
Pablo Torres <tn.pablo@gmail.com>
2014-02-25 16:18:24 -0500

    Better function names, some docstrings

--- src/guess/gen_arith.clj
+++ src/guess/gen_arith.clj
@@ -1,6 +1,6 @@
 (ns guess.gen-arith)
 
-(defn assoc-with [op]
+(defn assoc-op [op]
   (get {'clojure.core/* *
         'clojure.core/+ +
         'clojure.core/- +
@@ -10,74 +10,80 @@
 (defn commutative? [op]
   (#{'+ '*} op))
 
-(defn- assoc-chain [op x y]
+(defn- associate-chain
+  "Associate a chain of operations, e.g a * 2 * 2 -> a * 4"
+  [op x y]
   (cond
    (and (number? x)
         (coll? y)
         (= op (first y)))
    (if (number? (second y))
-     `(~op ~(last y) ~((assoc-with op) (second y) x))
+     `(~op ~(last y) ~((assoc-op op) (second y) x))
      (if (number? (last y))
-       `(~op ~(second y) ~((assoc-with op) (last y) x))))
+       `(~op ~(second y) ~((assoc-op op) (last y) x))))
 
    (and (number? y)
         (coll? x)
         (= op (first x)))
    (if (number? (second x))
-     `(~op ~(last x) ~((assoc-with op) (second x) y))
+     `(~op ~(last x) ~((assoc-op op) (second x) y))
      (if (number? (last x))
-       `(~op ~(second x) ~((assoc-with op) y (last x)))))))
+       `(~op ~(second x) ~((assoc-op op) y (last x)))))))
 
-(defn- assoc-add [x y]
-  (assoc-chain 'clojure.core/+ x y))
+(defn- simplify-add [x y]
+  (if-let [chained (associate-chain 'clojure.core/+ x y)]
+    chained
+    `(+ ~x ~y)))
 
-(defn- assoc-sub [x y]
-  (assoc-chain 'clojure.core/- x y))
+(defn- simplify-sub [x y]
+  (if-let [chained (associate-chain 'clojure.core/- x y)]
+    chained
+    `(- ~x ~y)))
 
-(defn- assoc-mult [x y]
-  (assoc-chain 'clojure.core/* x y))
+(defn- simplify-mult [x y]
+  (if-let [chained (associate-chain 'clojure.core/* x y)]
+    chained
+    `(* ~x ~y)))
 
-(defn- assoc-div [x y]
-  (assoc-chain 'clojure.core// x y))
+(defn- simplify-div [x y]
+  (if-let [chained (associate-chain 'clojure.core// x y)]
+    chained
+    `(/ ~x ~y)))
 
-(defn associate [op x y]
+(defn simplify
+  "Return an equivalent expression with less nesting."
+  [op x y]
   (case op
-    + (if-let [result (assoc-add x y)]
-        result
-        `(+ ~x ~y))
-    - (if-let [result (assoc-sub x y)]
-        result
-        `(- ~x ~y))
-    * (if-let [result (assoc-mult x y)]
-        result
-        `(* ~x ~y))
-    / (if-let [result (assoc-div x y)]
-        result
-        `(/ ~x ~y))
+    + (simplify-add x y)
+    - (simplify-sub x y)
+    * (simplify-mult x y)
+    / (simplify-div x y)
     `(~op ~x ~y)))
 
-(defn- build-exp [op x y]
+(defn- build-exp
+  "Short circuits operations. If not possible, delegate to simplify."
+  [op x y]
   (case op
     + (cond
        (= x 0) y
        (= y 0) x
        ;; prefer [* x 2] over [+ x x], but try to simplify first
-       (= x y) (associate '* x 2)
-       :else (associate '+ x y))
+       (= x y) (simplify '* x 2)
+       :else (simplify '+ x y))
     - (cond
        (= x y) 0
-       :else (associate '- ~x ~y))
+       :else (simplify '- ~x ~y))
     * (cond
        (or (= x 0) (= y 0)) 0
        (= x 1) y
        (= y 1) x
-       :else (associate '* x y))
+       :else (simplify '* x y))
     / (when-not (= y 0)
         (cond
          (= y 1) x
          (= x y) 1
-         :else (associate '/ ~x ~y)))
-    (associate op x y)))
+         :else (simplify '/ ~x ~y)))
+    (simplify op x y)))
 
 (defn- exps-with-ops
   [ops max-nesting numbers vars]
