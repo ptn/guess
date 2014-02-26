b6d9cc5bfc8f1666ba0c9486ffbe510625999fe3
Pablo Torres <tn.pablo@gmail.com>
2014-02-26 12:51:40 -0500

    Don't generate some expressions of the form (= x y)
    If x and y are themselves expressions that apply the same arithmetic
    operation to the same variable and with the same other operand, then
    don't build the expression, because (= x y) is obviously true.

--- src/guess/gen_bool.clj
+++ src/guess/gen_bool.clj
@@ -3,9 +3,62 @@
 (defn commutative? [op]
   (#{'=} op))
 
-(defn- build [op x y]
+(defn same-op?
+  "Tests whether x and y apply the same arithmetic operation to its operands."
+  [x y]
+  (and (coll? x)
+       (coll? y)
+       (= (first x) (first y))))
+
+;; TODO Unfuck this function.
+;; 1. Perform less comparisons.
+;; 2. This is tightly coupled to different-other-operand?, because its return
+;;    value is specifically tailored to be used by it.
+(defn same-var?
+  "Tests whether x and y apply an arithmetic operation to the same variable,
+e.g. '(+ a 8) and '(* 9 a). Returns the positions of the other operands."
+  [x y]
+  (cond
+   (and (symbol? (second x))
+        (symbol? (second y))
+        (= (second x) (second y)))
+   [2 2]
+
+   (and (symbol? (second x))
+        (symbol? (last y))
+        (= (second x) (last y)))
+   [2 1]
+
+   (and (symbol? (last x))
+        (symbol? (second y))
+        (= (last x) (second y)))
+   [1 2]
+
+   (and (symbol? (last x))
+        (symbol? (last y))
+        (= (last x) (last y)))
+   [1 1]))
+
+(defn different-other-operand?
+  "Verifies if x and y are expressions that apply and arithmetic operation
+to the same operands, e.g. '(+ a 8) '(- a 8) "
+  [x y pos-other-x pos-other-y]
+  (not (= (nth x pos-other-x) (nth y pos-other-y))))
+
+;; TODO Refactor.
+(defn build-equal? [x y]
+  (if (same-op? x y)
+    (if-let [[posx posy] (same-var? x y)]
+      (different-other-operand? x y posx posy)
+      true)
+    true))
+
+(defn build [op x y]
   (when-not (= x y)
-    `(~op ~x ~y)))
+    (case op
+      = (when (build-equal? x y)
+          `(= ~x ~y))
+      `(~op ~x ~y))))
 
 (defn all
   [& {:keys [ops max-nesting arith-exps]}]
